9787232ce34ea5c9b258bfcc233582c8bb22e21c
TehBrian <tehbrian@protonmail.com>
2021-09-13 20:23:15 -0400

    build: use Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("xyz.jpenilla.run-paper") version "1.0.4"
+}
+
+group = "xyz.tehbrian"
+version = "2.0.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+
+    maven("https://papermc.io/repo/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+    maven("https://s01.oss.sonatype.org/content/groups/public/") {
+        name = "sonatype-s01"
+    }
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+        name = "sonatype-s01-snapshots"
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+
+    implementation("com.google.inject:guice:5.0.1")
+
+    implementation("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT")
+    implementation("org.spongepowered:configurate-yaml:4.1.2")
+
+    implementation("cloud.commandframework:cloud-paper:1.5.0")
+
+    implementation("xyz.tehbrian.restrictionhelper:restrictionhelper-spigot:0.2.0-SNAPSHOT")
+    implementation("dev.tehbrian:tehlib-paper:0.1.0-SNAPSHOT")
+}
+
+tasks.processResources {
+    expand("version" to project.version)
+}
