7d6bc44fae11f8526956b91ebdeee697cdda2f9d
Brian <32250137+TehBrian@users.noreply.github.com>
2021-12-17 20:36:22 -0500

    build: target Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ group = "xyz.tehbrian"
 version = "2.0.0"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_16
-    targetCompatibility = JavaVersion.VERSION_16
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
