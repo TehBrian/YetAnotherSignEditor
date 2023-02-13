25e677a3363b4b3fab160e84d2b36b473d1afda7
TehBrian <tehbrian@proton.me>
2023-02-13 14:51:21 -0500

    mc 1.19.4; update deps

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # YetAnotherSignEditor
+
 Easily edit signs, with support for both MiniMessage and legacy formatting.
 
 * [Download](https://modrinth.com/plugin/yetanothersigneditor)
@@ -20,13 +21,16 @@ Above all, it just feels good to use! No bloat, no extra features.
 [MiniMessage]: https://docs.adventure.kyori.net/minimessage/
 
 ## Screenshots
+
 ![help menu](assets/help-menu.png)
 ![commands](assets/commands.png)
 
 ## Building
+
 This project uses Gradle. To build, run `./gradlew build` in the project's root
 directory. The built jar can be found in `build/libs`.
 
 ## Contributing
+
 Feel free to submit a pull request or file an issue! All changes are welcome. If
 you're contributing code, please follow the project's code style.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
   id("java")
-  id("com.github.johnrengelman.shadow") version "7.1.2"
-  id("xyz.jpenilla.run-paper") version "2.0.1"
+  id("com.github.johnrengelman.shadow") version "8.1.1"
+  id("xyz.jpenilla.run-paper") version "2.1.0"
   id("net.kyori.indra.checkstyle") version "3.0.1"
-  id("com.github.ben-manes.versions") version "0.44.0"
+  id("com.github.ben-manes.versions") version "0.46.0"
 }
 
 group = "xyz.tehbrian"
@@ -21,11 +21,11 @@ repositories {
 }
 
 dependencies {
-  compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+  compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
 
-  implementation("cloud.commandframework:cloud-minecraft-extras:1.8.0")
-  implementation("com.google.inject:guice:5.1.0")
-  implementation("dev.tehbrian:tehlib-paper:0.4.1")
+  implementation("cloud.commandframework:cloud-minecraft-extras:1.8.3")
+  implementation("com.google.inject:guice:7.0.0")
+  implementation("dev.tehbrian:tehlib-paper:0.4.2")
   implementation("org.spongepowered:configurate-yaml:4.1.2")
   implementation("xyz.tehbrian.restrictionhelper:restrictionhelper-spigot:0.3.2")
 }
@@ -55,6 +55,6 @@ tasks {
   }
 
   runServer {
-    minecraftVersion("1.19.3")
+    minecraftVersion("1.19.4")
   }
 }
