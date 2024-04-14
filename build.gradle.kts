9c233b1012152413bd761c8ae0778d0105b93706
TehBrian <tehbrian@proton.me>
2024-04-14 11:47:02 -0400

    mc 1.20.4; update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
   id("java")
   id("com.github.johnrengelman.shadow") version "8.1.1"
-  id("xyz.jpenilla.run-paper") version "2.1.0"
+  id("xyz.jpenilla.run-paper") version "2.3.0"
   id("net.kyori.indra.checkstyle") version "3.1.3"
-  id("com.github.ben-manes.versions") version "0.47.0"
+  id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 group = "dev.tehbrian"
@@ -21,10 +21,10 @@ repositories {
 }
 
 dependencies {
-  compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+  compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
 
-  implementation("cloud.commandframework:cloud-paper:1.8.3")
-  implementation("cloud.commandframework:cloud-minecraft-extras:1.8.3")
+  implementation("cloud.commandframework:cloud-paper:1.8.4")
+  implementation("cloud.commandframework:cloud-minecraft-extras:1.8.4")
   implementation("com.google.inject:guice:7.0.0")
   implementation("dev.tehbrian:tehlib-paper:0.5.0")
   implementation("org.spongepowered:configurate-yaml:4.1.2")
@@ -56,6 +56,6 @@ tasks {
   }
 
   runServer {
-    minecraftVersion("1.20.1")
+    minecraftVersion("1.20.4")
   }
 }
