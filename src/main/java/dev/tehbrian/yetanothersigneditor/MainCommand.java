4be25c64a68c6cc426629d8628d56ffae0fe4887
TehBrian <tehbrian@proton.me>
2024-07-21 12:17:44 -0400

    clean up command descriptions

--- src/main/java/dev/tehbrian/yetanothersigneditor/MainCommand.java
+++ src/main/java/dev/tehbrian/yetanothersigneditor/MainCommand.java
@@ -17,12 +17,20 @@ import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.Effect;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.block.sign.Side;
+import org.bukkit.block.sign.SignSide;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.spongepowered.configurate.NodePath;
 import xyz.tehbrian.restrictionhelper.core.ActionType;
 import xyz.tehbrian.restrictionhelper.spigot.SpigotRestrictionHelper;
+
+import java.util.List;
+
+import static dev.tehbrian.yetanothersigneditor.SignFormatting.MAGIC_NUMBER_OF_TICKS;
+import static dev.tehbrian.yetanothersigneditor.SignFormatting.shouldFormat;
+import static dev.tehbrian.yetanothersigneditor.SignFormatting.unformatLines;
 
 public final class MainCommand {
 
@@ -51,123 +59,8 @@ public final class MainCommand {
     final var help = main
         .handler(c -> c.getSender().sendMessage(this.langConfig.c(NodePath.path("help"))));
 
-    final var reload = main
-        .literal("reload", ArgumentDescription.of("Reload the plugin's config."))
-        .permission(Permission.RELOAD)
-        .handler(c -> {
-          if (this.yetAnotherSignEditor.loadConfiguration()) {
-            c.getSender().sendMessage(this.langConfig.c(NodePath.path("reload", "successful")));
-          } else {
-            c.getSender().sendMessage(this.langConfig.c(NodePath.path("reload", "unsuccessful")));
-          }
-        });
-
-    final var unwax = main
-        .literal("unwax", ArgumentDescription.of("Unwax the sign you're looking at."))
-        .permission(Permission.UNWAX)
-        .senderType(Player.class)
-        .handler(c -> {
-          final Player player = (Player) c.getSender();
-
-          final @Nullable Block targetedBlock = player.getTargetBlockExact(6);
-          if (targetedBlock == null || !(targetedBlock.getState() instanceof final Sign sign)) {
-            player.sendMessage(this.langConfig.c(NodePath.path("not-a-sign")));
-            return;
-          }
-
-          if (!this.restrictionHelper.checkRestrictions(player, targetedBlock.getLocation(), ActionType.ALL)) {
-            player.sendMessage(this.langConfig.c(NodePath.path("no-permission-here")));
-            return;
-          }
-
-          if (!sign.isWaxed()) {
-            player.sendMessage(this.langConfig.c(NodePath.path("not-waxed")));
-            return;
-          }
-
-          sign.setWaxed(false);
-          sign.update();
-          sign.getWorld().playEffect(sign.getLocation(), Effect.COPPER_WAX_OFF, 0);
-          sign.getWorld().playSound(Sound.sound(
-              org.bukkit.Sound.ITEM_HONEYCOMB_WAX_ON,
-              Source.BLOCK,
-              1.0F, 1.0F
-          ));
-          sign.getWorld().playSound(Sound.sound(
-              org.bukkit.Sound.ITEM_BOTTLE_EMPTY,
-              Source.BLOCK,
-              1.0F, 0.7F
-          ));
-        });
-
-    final var open = main
-        .literal("open", ArgumentDescription.of("Forcibly open the targeted sign for editing."))
-        .senderType(Player.class)
-        .handler(c -> {
-          final Player player = (Player) c.getSender();
-          final User user = this.userService.getUser(player);
-
-          final @Nullable Block targetedBlock = player.getTargetBlockExact(6);
-          if (targetedBlock == null || !(targetedBlock.getState() instanceof final Sign sign)) {
-            player.sendMessage(this.langConfig.c(NodePath.path("not-a-sign")));
-            return;
-          }
-
-          if (!this.restrictionHelper.checkRestrictions(player, targetedBlock.getLocation(), ActionType.ALL)) {
-            player.sendMessage(this.langConfig.c(NodePath.path("no-permission-here")));
-            return;
-          }
-
-          final Side side = sign.getInteractableSideFor(player);
-          final SignSide signSide = sign.getSide(side);
-
-          if (shouldFormat(user)) {
-            final List<Component> newLines = unformatLines(sign, side, user);
-            for (int i = 0; i < newLines.size(); i++) {
-              signSide.line(i, newLines.get(i));
-            }
-            sign.update();
-
-            this.yetAnotherSignEditor.getServer().getScheduler().runTaskLater(
-                this.yetAnotherSignEditor,
-                () -> player.openSign(sign, side),
-                MAGIC_NUMBER_OF_TICKS
-            );
-          } else {
-            player.openSign(sign, side);
-          }
-        });
-
-    final var format = main
-        .literal("format", ArgumentDescription.of("Toggle your ability to format sign text."))
-        .permission(Permission.FORMAT)
-        .senderType(Player.class)
-        .handler(c -> {
-          final Player sender = (Player) c.getSender();
-          if (this.userService.getUser(sender).toggleFormatEnabled()) {
-            sender.sendMessage(this.langConfig.c(NodePath.path("format", "enabled")));
-          } else {
-            sender.sendMessage(this.langConfig.c(NodePath.path("format", "disabled")));
-          }
-        });
-
-    final var formatFormattingType = format
-        .argument(EnumArgument
-            .<CommandSender, User.FormattingType>builder(User.FormattingType.class, "formatting_type")
-            .build())
-        .handler(c -> {
-          final Player player = (Player) c.getSender();
-          final User.FormattingType formattingType = c.get("formatting_type");
-
-          this.userService.getUser(player).formattingType(formattingType);
-          player.sendMessage(this.langConfig.c(
-              NodePath.path("format", "set"),
-              Placeholder.parsed("formatting_type", formattingType.pretty())
-          ));
-        });
-
     final var set = main
-        .literal("set", ArgumentDescription.of("Set the text of the sign you're looking at."))
+        .literal("set", ArgumentDescription.of("Set the text of the targeted sign."))
         .permission(Permission.SET)
         .senderType(Player.class)
         .argument(IntegerArgument.<CommandSender>builder("line").withMin(1).withMax(4).build())
@@ -210,12 +103,128 @@ public final class MainCommand {
           ));
         });
 
+    final var open = main
+        .literal("open", ArgumentDescription.of("Open the targeted sign."))
+        .senderType(Player.class)
+        .handler(c -> {
+          final Player player = (Player) c.getSender();
+          final User user = this.userService.getUser(player);
+
+          final @Nullable Block targetedBlock = player.getTargetBlockExact(6);
+          if (targetedBlock == null || !(targetedBlock.getState() instanceof final Sign sign)) {
+            player.sendMessage(this.langConfig.c(NodePath.path("not-a-sign")));
+            return;
+          }
+
+          if (!this.restrictionHelper.checkRestrictions(player, targetedBlock.getLocation(), ActionType.ALL)) {
+            player.sendMessage(this.langConfig.c(NodePath.path("no-permission-here")));
+            return;
+          }
+
+          final Side side = sign.getInteractableSideFor(player);
+          final SignSide signSide = sign.getSide(side);
+
+          if (shouldFormat(user)) {
+            final List<Component> newLines = unformatLines(sign, side, user);
+            for (int i = 0; i < newLines.size(); i++) {
+              signSide.line(i, newLines.get(i));
+            }
+            sign.update();
+
+            this.yetAnotherSignEditor.getServer().getScheduler().runTaskLater(
+                this.yetAnotherSignEditor,
+                () -> player.openSign(sign, side),
+                MAGIC_NUMBER_OF_TICKS
+            );
+          } else {
+            player.openSign(sign, side);
+          }
+        });
+
+    final var unwax = main
+        .literal("unwax", ArgumentDescription.of("Unwax the targeted sign."))
+        .permission(Permission.UNWAX)
+        .senderType(Player.class)
+        .handler(c -> {
+          final Player player = (Player) c.getSender();
+
+          final @Nullable Block targetedBlock = player.getTargetBlockExact(6);
+          if (targetedBlock == null || !(targetedBlock.getState() instanceof final Sign sign)) {
+            player.sendMessage(this.langConfig.c(NodePath.path("not-a-sign")));
+            return;
+          }
+
+          if (!this.restrictionHelper.checkRestrictions(player, targetedBlock.getLocation(), ActionType.ALL)) {
+            player.sendMessage(this.langConfig.c(NodePath.path("no-permission-here")));
+            return;
+          }
+
+          if (!sign.isWaxed()) {
+            player.sendMessage(this.langConfig.c(NodePath.path("not-waxed")));
+            return;
+          }
+
+          sign.setWaxed(false);
+          sign.update();
+          sign.getWorld().playEffect(sign.getLocation(), Effect.COPPER_WAX_OFF, 0);
+          sign.getWorld().playSound(Sound.sound(
+              org.bukkit.Sound.ITEM_HONEYCOMB_WAX_ON,
+              Source.BLOCK,
+              1.0F, 1.0F
+          ));
+          sign.getWorld().playSound(Sound.sound(
+              org.bukkit.Sound.ITEM_BOTTLE_EMPTY,
+              Source.BLOCK,
+              1.0F, 0.7F
+          ));
+        });
+
+    final var format = main
+        .literal("format", ArgumentDescription.of("Toggle text formatting or change your formatting type."))
+        .permission(Permission.FORMAT)
+        .senderType(Player.class)
+        .handler(c -> {
+          final Player sender = (Player) c.getSender();
+          if (this.userService.getUser(sender).toggleFormatEnabled()) {
+            sender.sendMessage(this.langConfig.c(NodePath.path("format", "enabled")));
+          } else {
+            sender.sendMessage(this.langConfig.c(NodePath.path("format", "disabled")));
+          }
+        });
+
+    final var formatFormattingType = format
+        .argument(EnumArgument
+            .<CommandSender, User.FormattingType>builder(User.FormattingType.class, "formatting_type")
+            .build())
+        .handler(c -> {
+          final Player player = (Player) c.getSender();
+          final User.FormattingType formattingType = c.get("formatting_type");
+
+          this.userService.getUser(player).formattingType(formattingType);
+          player.sendMessage(this.langConfig.c(
+              NodePath.path("format", "set"),
+              Placeholder.parsed("formatting_type", formattingType.pretty())
+          ));
+        });
+
+    final var reload = main
+        .literal("reload", ArgumentDescription.of("Reload the plugin's config."))
+        .permission(Permission.RELOAD)
+        .handler(c -> {
+          if (this.yetAnotherSignEditor.loadConfiguration()) {
+            c.getSender().sendMessage(this.langConfig.c(NodePath.path("reload", "successful")));
+          } else {
+            c.getSender().sendMessage(this.langConfig.c(NodePath.path("reload", "unsuccessful")));
+          }
+        });
+
     commandManager.command(help)
-        .command(reload)
+        .command(set)
+        .command(open)
         .command(unwax)
         .command(format)
         .command(formatFormattingType)
-        .command(set);
+        .command(reload);
   }
 
 }
