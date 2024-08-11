e1345f9299697217473f916f7df7b8d45e042676
TehBrian <tehbrian@proton.me>
2024-08-11 16:00:54 -0400

    upgrade to RH 0.4.1

--- src/main/java/dev/tehbrian/yetanothersigneditor/inject/SingletonModule.java
+++ src/main/java/dev/tehbrian/yetanothersigneditor/inject/SingletonModule.java
@@ -1,9 +1,9 @@
 package dev.tehbrian.yetanothersigneditor.inject;
 
 import com.google.inject.AbstractModule;
+import dev.tehbrian.restrictionhelper.spigot.SpigotRestrictionHelper;
 import dev.tehbrian.yetanothersigneditor.config.LangConfig;
 import dev.tehbrian.yetanothersigneditor.user.UserService;
-import dev.tehbrian.restrictionhelper.spigot.SpigotRestrictionHelper;
 
 public final class SingletonModule extends AbstractModule {
 
